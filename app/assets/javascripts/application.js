b1cc90f01fdcffeacdf7a8bf628fc5e22d7071ec
Dunya Kirkali <dunyakirkali@gmail.com>
2013-06-20 11:58:46 +0200

    Ported fiddle to rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,104 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery.isotope
+//= require jquery.transit
 //= require_tree .
+
+$(document).ready(function () {
+  $("#header").hide();
+  var list = $("#list");
+  var question_one = "Zou je het heel gek vinden als ik je verkering zou vragen?";
+  var question_two = "Wil je met me gaan?";
+  var question_one_words = question_one.split(" ");
+  var question_two_words = question_two.split(" ");
+
+  $.each(question_one_words, function (index, obj) {
+    list.append($('<div class="item curved q-one ' + randomColor() + '">' + obj + "</div>"));
+  });
+
+  $.each(question_two_words, function (index, obj) {
+    list.append($('<div class="item curved q-two ' + randomColor() + '">' + obj + "</div>"));
+  });
+
+  list.isotope({
+    itemSelector: '.item',
+    filter: '.q-one'
+  });
+
+  list.isotope('shuffle');
+
+  $(document).on('mouseover', ".item", function () {
+    $(this).transition({
+      opacity: 0.2
+    }, 200, 'easeInOutQuad', function () {
+      setTimeout(function () {
+        list.isotope('reLayout');
+      }, 100);
+    });
+
+  });
+  $(document).on('mouseleave', ".item", function () {
+    $(this).transition({
+      opacity: 1
+    }, 200, 'easeInOutQuad', function () {
+      setTimeout(function () {
+        list.isotope('reLayout');
+      }, 100);
+    });
+  });
+
+  $("#nee").on("click", updateQuestion);
+
+  $("#ja").on("mouseover", function (event) {
+    list.isotope('shuffle');
+  });
+});
+
+function randomColor() {
+  var colors = ["dark-red", "medium-dark-red", "medium-red", "medium-ligth-red", "light-red"];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function updateQuestion() {
+  var list = $("#list");
+  list.isotope({
+    filter: ".q-two"
+  });
+  list.isotope({
+    sortBy: 'original-order'
+  });
+  $("#nee").off("click");
+  $("#nee").on("click", shuffle);
+  $("#ja").off("mouseover");
+  $("#ja").off("click");
+  $("#ja").on("click", complete);
+}
+
+function shuffle() {
+
+  var list = $("#list");
+  list.isotope('shuffle');
+}
+
+function complete() {
+  $("#buttons").slideUp();
+  $("#header").slideDown();
+  $("#meer").on("click", addMoreLove);
+  $("#list").isotope('remove', $(".item"), addMoreLove);
+}
+
+function getRandomLoveWord() {
+  var love_words = "lief aardig schattig sexy dirty wild liefdevol lekker zoet uniek kleurvol actief uitstekend ♥ wit gek";
+  var words = love_words.split(" ");
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function addMoreLove() {
+  var list = $("#list");
+  var word = getRandomLoveWord();
+  list.isotope({
+    filter: ".love-word"
+  });
+  list.isotope("insert", $('<div class="item curved love-word ' + randomColor() + '">' + word + "</div>"));
+}

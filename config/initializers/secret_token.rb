63c9b784940bb54a8c604019a496b8be5c3b56ac
Dunya Kirkali <dunyakirkali@gmail.com>
2013-06-20 11:15:13 +0200

    Initial rails project without tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Verkering::Application.config.secret_key_base = '994a8907818c71509d5fa83929dbd027744717c32087ed84c77f813fbf3acf3fb83bb1075ee4055159817b58bed8fdb88bab8fd7c7f7a911805818d7583f7f56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Verkering::Application.config.session_store :cookie_store, key: '_verkering_session'
